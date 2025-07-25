92bed373256e1319385b21722b11efe9fcd2a5e4
Jérémy Ansel <JeremyAnsel@users.noreply.github.com>
2025-07-25 21:52:43 +0200

    Add tests

--- JeremyAnsel.IO.Locator/JeremyAnsel.IO.Locator.Tests/FileLocatorTests.cs
+++ JeremyAnsel.IO.Locator/JeremyAnsel.IO.Locator.Tests/FileLocatorTests.cs
@@ -5,41 +5,40 @@
 // Licensed under the MIT license. See LICENSE.txt
 // </license>
 
-namespace JeremyAnsel.IO.Locator.Tests
+namespace JeremyAnsel.IO.Locator.Tests;
+
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using SharpCompress.Common;
+using Xunit;
+
+[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed")]
+public class FileLocatorTests
 {
-    using System.Diagnostics.CodeAnalysis;
-    using System.IO;
-    using System.Linq;
-    using SharpCompress.Common;
-    using Xunit;
-
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed")]
-    public class FileLocatorTests
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
+        var temp = Path.GetTempFileName();
+
+        try
         {
-            var temp = Path.GetTempFileName();
-
-            try
+            for (int pass = 0; pass < 2; pass++)
             {
-                for (int pass = 0; pass < 2; pass++)
+                using (var zip = WritableFileLocatorFactory.CreateArchive(temp, ArchiveType.Zip, CompressionType.LZMA))
                 {
-                    using (var zip = WritableFileLocatorFactory.CreateArchive(temp, ArchiveType.Zip, CompressionType.LZMA))
-                    {
-                        zip.Create(Path.Combine("a", Path.GetRandomFileName()));
-                    }
-                }
-
-                using (var reader = FileLocatorFactory.Create(temp))
-                {
-                    Assert.Equal(2, reader.EnumerateFiles().Count());
+                    zip.Create(Path.Combine("a", Path.GetRandomFileName()));
                 }
             }
-            finally
+
+            using (var reader = FileLocatorFactory.Create(temp))
             {
-                File.Delete(temp);
+                Assert.Equal(2, reader.EnumerateFiles().Count());
             }
+        }
+        finally
+        {
+            File.Delete(temp);
         }
     }
 }
---
+++ JeremyAnsel.IO.Locator/JeremyAnsel.IO.Locator.Tests/MemoryFileLocatorTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Xunit;
+
+namespace JeremyAnsel.IO.Locator.Tests;
+
+[SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:ElementsMustBeDocumented", Justification = "Reviewed")]
+public class MemoryFileLocatorTests
+{
+    [Fact]
+    public void New_IsEmpty()
+    {
+        using var locator = new MemoryFileLocator();
+
+        Assert.True(locator.IsEmpty);
+    }
+
+    [Fact]
+    public void Clear_IsEmpty()
+    {
+        using var locator = new MemoryFileLocator();
+        locator.Create("test1");
+
+        locator.Clear();
+
+        Assert.True(locator.IsEmpty);
+    }
+
+    [Fact]
+    public void Create_Valid()
+    {
+        using var locator = new MemoryFileLocator();
+        locator.Create("test1");
+        locator.Create("test2");
+
+        var files = locator.EnumerateFiles().ToList();
+
+        Assert.Equal(2, files.Count);
+        Assert.Equal("test1", files[0]);
+        Assert.Equal("test2", files[1]);
+    }
+
+    [Fact]
+    public void Create_Null()
+    {
+        using var locator = new MemoryFileLocator();
+        Assert.Throws<ArgumentNullException>(() => locator.Create(string.Empty));
+    }
+
+    [Fact]
+    public void Create_Duplicate()
+    {
+        using var locator = new MemoryFileLocator();
+        locator.Create("test1");
+        Assert.Throws<ArgumentOutOfRangeException>(() => locator.Create("test1"));
+    }
+
+    [Fact]
+    public void Exists()
+    {
+        using var locator = new MemoryFileLocator();
+        locator.Create("test1");
+        locator.Create("test2");
+
+        Assert.True(locator.Exists("test1"));
+        Assert.True(locator.Exists("test2"));
+    }
+}
